a036e17a91e7769d0d824e4dc9b1993aa676b155
Jin-7793 <soccer4484@gmail.com>
2023-07-04 21:27:56 +0900

    バリデーション追加

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,28 +1,29 @@
-<!DOCTYPE html>
-<html lang=="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Blog</title>
-        <!--Fonts-->
+        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog Name</h1>
-            <form action='/posts' method='POST'>
-                @csrf
-                <div class='title'>
-                    <h2>title</h2>
-                    <input type='text' name='post[title]' placeholder='本日の記録'>
-                </div>
-                <div class='post'>
-                    <h2>body</h2>
-                    <textarea type="text" name='post[body]' placeholder='はらへったなぁ。'></textarea>
-                </div>
-                <input type='submit' value='保存'>
-            </form>
-        <p class="button">
-            <button type="button" onClick="history.back()">戻る</button>
-        </p>
+        <h1>BlogName</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name=post[title] placeholder="タイトル" value="{{ old('post.title')}}"/>
+                <p class="title__errors" style="color:red">{{$errors->first('post.title')}}</p> 
+            </div>
+            <div class="Body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="本文">{{ old('post.body') }}</textarea>
+                <p class="title__errors" style="color:red">{{$errors->first('post.body')}}</p>
+            </div>
+                <input type="submit" value="store">
+        </form>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
     </body>
 </html>
